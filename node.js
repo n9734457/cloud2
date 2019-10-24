d9276cfd04c8d71cca927f570173cf796c9fa017
Lachlan Stewart <lachlanstewart6@gmail.com>
2019-10-24 11:47:02 +1000

    create chart html

--- node.js
+++ node.js
@@ -41,6 +41,58 @@ client.get('search/tweets', {q: '#incident', lang: 'en', count: 100}, function(e
      })
  });
 
+ function createChart(dataValues) {
+    const head = '<canvas id="myChart" width="400" height="400"></canvas>' +
+    "<script>" +
+    "var ctx = document.getElementById('myChart');" +
+    "var myChart = new Chart(ctx, {" +
+        "type: 'bar'," +
+        "data: {" +
+            "labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']," +
+            "datasets: [{" +
+                "label: '# of Votes', " +
+                "data: [";
+
+    const tail = "]," + 
+            "backgroundColor: [" +
+              "'rgba(255, 99, 132, 0.2)'," +
+                "'rgba(54, 162, 235, 0.2)'," +
+                "'rgba(255, 206, 86, 0.2)'," +
+                "'rgba(75, 192, 192, 0.2)'," +
+                "'rgba(153, 102, 255, 0.2)'," +
+                "'rgba(255, 159, 64, 0.2)'" +
+            "]," +
+            "borderColor: [" +
+                "'rgba(255, 99, 132, 1)'," +
+                "'rgba(54, 162, 235, 1)'," +
+                "'rgba(255, 206, 86, 1)'," +
+                "'rgba(75, 192, 192, 1)'," +
+                "'rgba(153, 102, 255, 1)'," +
+                "'rgba(255, 159, 64, 1)'" +
+            "]," +
+            "borderWidth: 1" +
+        "}]" +
+    "},"
+    "options: {" +
+        "scales: {" +
+            "yAxes: [{" +
+                "ticks: {" +
+                    "beginAtZero: true" +
+                "}" +
+            "}]" +
+        "}" +
+    "}" +
+"});" +
+"</script>";
+
+const valueString; 
+
+dataValues.forEach(value => {
+    valueString += value + ',';
+});
+
+    return head + valueString + tail;
+}
 
 app.listen(3000, () => {
     console.log('Server listening on port: ', 3000);
